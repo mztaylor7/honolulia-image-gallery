41b0d2070130a20b57ff838d04c0064293c16606
Michael Taylor <cardzfan1207@gmail.com>
2020-06-17 22:48:03 -0400

    Updated with initial file structure

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var path = require('path');
+var SRC_DIR = path.join(__dirname, '/client/src');
+var DIST_DIR = path.join(__dirname, '/client/dist');
+
+module.exports = {
+  entry: `${SRC_DIR}/index.js`,
+  output: {
+    filename: 'bundle.js',
+    path: DIST_DIR
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/,
+        include: SRC_DIR,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  }
+}
